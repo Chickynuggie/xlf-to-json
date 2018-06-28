5c9c36c22b20f926c447c33718fa2ba6ea1cbd7f
Daniel Farkas <daniel.farkas@oce.com>
2018-06-28 15:25:06 +0200

    extend gulp task to rename files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
-var xliff2json = require('./index.js')
-var gulp = require('gulp')
+var xliff2json = require('./index.js');
+var gulp = require('gulp');
+var rename = require("gulp-rename");
 
-gulp.task('xliff2json', function() {
- return gulp.src('./to-translate/**/*.xlf')
- .pipe(xliff2json())
- .pipe(gulp.dest('./output'))
+gulp.task('xliff2json', function () {
+    return gulp.src('./to-translate/**/*.xlf')
+        .pipe(xliff2json())
+        .pipe(rename(function (opt) {
+            opt.basename = opt.basename.replace(/^web_texts_/, '');
+            return opt;
+        }))
+        .pipe(gulp.dest('./output'))
 })
